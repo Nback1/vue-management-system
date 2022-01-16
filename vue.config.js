e7f6471c6bad3ded0880c4aaaa9a6e3163757ab5
Nback1 <394379336@qq.com>
2022-01-16 21:49:17 +0800

    解决了vue.config.js配置文件的问题，并不是不生效而是写抄了一个字母导致，通过百度搜索错误提示信息，然后console.log发现名字写错了，在此之前怎么也搜不到想要的答案，所以说一定是犯了最简单的错误！

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  // chainWebpack: config => {
-  //   config.when(process.env.NOOE_ENV === 'production', config => {
-  //     config.entry('app').clear().add('./src/main-prod.js')
-  //   })
-  //   config.when(process.env.NOOE_ENV === 'development', config => {
-  //     config.entry('app').clear().add('./src/main-dev.js')
-  //   })
-  // }
+  chainWebpack: config => {
+    // console.log(config)
+    // console.log(process)
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+    })
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
 }
